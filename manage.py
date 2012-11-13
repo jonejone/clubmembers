8f6e37c054817a29b95a347b22aa8cef1230e636
Jone Eide <jone@idev.no>
2012-11-14 00:08:44 +0100

    Added some Django files

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "clubmembmers.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "clubmembers.settings")
 
     from django.core.management import execute_from_command_line
 
