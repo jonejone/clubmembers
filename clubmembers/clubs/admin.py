07b65e3829e19c4377260c98e11eb5d70d014362
Jone Eide <jone@idev.no>
2013-02-05 13:11:16 +0100

    Added more Club admin details

--- clubmembers/clubs/admin.py
+++ clubmembers/clubs/admin.py
@@ -1,6 +1,19 @@
 from django.contrib import admin
-from clubmembers.clubs.models import Club, ClubRegion
+from clubmembers.clubs.models import (  Club,
+                                        ClubRegion,
+                                        ClubSite,
+                                        ClubAdmin)
+
+
+class ClubSiteAdmin(admin.ModelAdmin):
+    list_display = ('club', 'site')
+
+
+class ClubAdminAdmin(admin.ModelAdmin):
+    list_display = ('club', 'user')
 
 
 admin.site.register(Club)
 admin.site.register(ClubRegion)
+admin.site.register(ClubSite, ClubSiteAdmin)
+admin.site.register(ClubAdmin, ClubAdminAdmin)
