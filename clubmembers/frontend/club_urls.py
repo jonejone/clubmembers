bb15297079e314777c86d7052e25e4d89e4f8a3f
Jone Eide <jone@idev.no>
2013-02-05 13:12:10 +0100

    Improved club views

--- clubmembers/frontend/club_urls.py
+++ clubmembers/frontend/club_urls.py
@@ -1,11 +1,18 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from dynamicsiteslite.views import site_info
 admin.autodiscover()
 
 urlpatterns = patterns('clubmembers.frontend.club_views',
     url(r'^$', 'index', name='club-index'),
     url(r'^register/$', 'register', name='club-register'),
     url(r'^member/(?P<member_id>\d)/$', 'member', name='club-member'),
+    url(r'^member/(?P<member_id>\d)/edit/$', 'member_edit', name='club-member-edit'),
+
+    url(r'^site-info$', site_info),
+
+    url(r'^accounts/members/', 'account_members', name='club-account-members'),
+    url(r'^accounts/', include('clubmembers.frontend.registration_urls')),
 )
 
 urlpatterns += patterns('',
--- clubmembers/frontend/club_views.py
+++ clubmembers/frontend/club_views.py
@@ -1,21 +1,35 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 from django.conf import settings
 from django.forms.models import modelformset_factory
+from django.core.urlresolvers import reverse
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 
 from datetime import date
 
 from clubmembers.clubs.models import Club
 from clubmembers.members.models import Member
-from clubmembers.members.forms import RegisterMemberForm
+from clubmembers.members.forms import (RegisterMemberForm,
+                                       MemberPaymentForm,)
+
+
+@login_required
+def account_members(request):
+    tmpl_dict = {
+        'members': request.user.member_set.all(),
+    }
+
+    return render(
+        request,
+        'club/account/members.html',
+        tmpl_dict)
+
 
 def index(request):
-    club = Club.objects.get(id=settings.CLUB_ID)
     members = Member.objects.filter(
-        club_region__club=club)
+        club_region__club=request.club)
 
     data = {
-        'club': club,
         'members': members,
     }
 
@@ -23,32 +37,37 @@ def index(request):
 
 
 def register(request):
-    club = Club.objects.get(id=settings.CLUB_ID)
 
     if request.method == 'POST':
         form = RegisterMemberForm(request.POST,
             request.FILES)
         if form.is_valid():
             new_member = form.save(commit=False)
-            new_member.club = club
+            new_member.club = request.club
+
+            if request.user.is_authenticated():
+                new_member.added_by = request.user
+
             new_member.save()
             return HttpResponseRedirect('/?saved=%i' % new_member.id)
     else:
         form = RegisterMemberForm()
 
-    form.fields['club_region'].queryset = club.clubregion_set.all()
+    form.fields['club_region'].queryset = request.club.clubregion_set.all()
     form.fields['joined'].initial = date.today()
 
     data = {
-        'club': club,
         'form': form,
     }
 
     return render(request, 'club/register.html', data)
 
 
-def member(request, member_id):
-    club = Club.objects.get(id=settings.CLUB_ID)
+def member_edit(request, member_id):
+    club = request.club
+
+    if not request.is_club_admin:
+        raise Http404
 
     try:
         member = Member.objects.filter(
@@ -56,9 +75,55 @@ def member(request, member_id):
     except IndexError:
         raise Http404
 
+    if request.method == 'POST':
+        form = RegisterMemberForm(
+            request.POST,
+            instance=member,)
+
+        if form.is_valid():
+            new_member = form.save()
+            return HttpResponseRedirect(reverse(
+                'club-member', args=[new_member.id]))
+    else:
+        form = RegisterMemberForm(instance=member)
+
+    form.fields['club_region'].queryset = request.club.clubregion_set.all()
+    form.fields['joined'].initial = date.today()
+
+    tmpl_dict = {
+        'member': member,
+        'form': form,
+    }
+
+    return render(request,
+        'club/member/edit.html', tmpl_dict)
+
+
+def member(request, member_id):
+    club = request.club
+
+    try:
+        member = Member.objects.filter(
+            id=member_id, club_region__club=club)[0]
+    except IndexError:
+        raise Http404
+
+    if request.method == 'POST':
+        payment_form = MemberPaymentForm(request.POST,
+            member=member)
+
+        if payment_form.is_valid():
+            payment = payment_form.save()
+            return HttpResponseRedirect(
+                '%s?payment_saved=1' % reverse(
+                    'club-member', args=[member.id, ]))
+    else:
+        payment_form = MemberPaymentForm(
+            member=member)
+
     data = {
         'member': member,
-        'club': club,
+        'payment_form': payment_form,
     }
 
     return render(request, 'club/member.html', data)
