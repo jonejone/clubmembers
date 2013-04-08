1dbe8a3f6742eee444335e955d0f7182c72fafe4
Jone Eide <jone@idev.no>
2013-04-09 01:28:31 +0200

    Fixed bad regex in clubs/urls

--- clubmembers/clubs/urls.py
+++ clubmembers/clubs/urls.py
@@ -5,8 +5,8 @@ admin.autodiscover()
 urlpatterns = patterns('clubmembers.frontend.club_views',
     url(r'^$', 'index', name='club-index'),
     url(r'^register/$', 'register', name='club-register'),
-    url(r'^member/(?P<member_id>\d)/$', 'member', name='club-member'),
-    url(r'^member/(?P<member_id>\d)/edit/$', 'member_edit', name='club-member-edit'),
+    url(r'^member/(?P<member_id>\d+)/$', 'member', name='club-member'),
+    url(r'^member/(?P<member_id>\d+)/edit/$', 'member_edit', name='club-member-edit'),
 
     url(r'^accounts/members/', 'account_members', name='club-account-members'),
     url(r'^accounts/', include('clubmembers.frontend.registration_urls')),
